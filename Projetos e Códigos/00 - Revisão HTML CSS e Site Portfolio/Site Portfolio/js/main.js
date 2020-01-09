13a1c5767c9e38f142c737a25ca468b0dbdab9ce
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-01-09 02:30:52 -0300

    Mudanças nas variáveis de  para .

--- Projetos e Códigos/00 - Revisão HTML CSS e Site Portfolio/Site Portfolio/js/main.js
+++ Projetos e Códigos/00 - Revisão HTML CSS e Site Portfolio/Site Portfolio/js/main.js
@@ -1,19 +1,19 @@
-var menu = document.querySelector('.menu');
-var hamIcon = document.querySelector('.ham-icon');
-var close = document.querySelector('.close');
+let menu = document.querySelector('.menu');
+let hamIcon = document.querySelector('.ham-icon');
+let close = document.querySelector('.close');
 
-var openMenu = function () {
+let openMenu = function () {
     menu.style.display = "block";
     hamIcon.style.display = "none";
     close.style.display = "block";
 }
 
-var closeMenu = function(){
+let closeMenu = function(){
     menu.style.display = "none";
     hamIcon.style.display = "block";
     close.style.display = "none";
 }
 
-
 hamIcon.addEventListener('click', openMenu);
 close.addEventListener('click', closeMenu);
+
