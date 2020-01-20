af4024cf1e7caeb0ec6b3bc7a70fe93aef53e563
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-01-19 22:52:19 -0300

    Testando novas formas de uso de vetores JS.

--- Projetos e Códigos/05 - Desafio Folha SP/espaconave.js
+++ Projetos e Códigos/05 - Desafio Folha SP/espaconave.js
@@ -1,2 +1,6 @@
-var stars = ['SIRIUS', 'LALANDE', 'PROCION', 'ALPHA', 'CENTAURI', 'BARNARD'];
+var nameStars = ['SIRIUS', 'LALANDE', 'PROCION', 'ALPHA', 'CENTAURI', 'BARNARD'];
+var stars = [SIRIUS, LALANDE, PROCION, ALPHA, CENTAURI, BARNARD] = nameStars;
 
+stars.forEach(name => {
+    console.log(name);
+})
